52f568ba323573674697c392ec01f24295798a69
shddnrwls <shddnrwls@gmail.com>
2018-05-13 16:50:10 +0900

    constant&variable

---
+++ Constantvariable.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import Swift
+
+let constant:String = "변경할 수 없는 상수입니다."
+var variable:String = "변경할 수 있는 변수입니다."
+
+//constant = "변경을 하려고 하면 에러가납니다."
+variable = "다른 값을 넣어도 에러가 나지 않습니다."
+
+//변수에 값을 할당 안하고 출력을 하면 에러가납니다.
+var user :String
+//print(user) 에러납니다.
+user = "뚜룩"
+print(user) //user라는 변수에 뚜룩이라는 값을 할당해준상태라 에러가 나지 않습니다.
+
