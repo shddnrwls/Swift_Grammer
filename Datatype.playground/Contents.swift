69b83bc2a72de8b29ffdceb5af5d70b9cb7f4acc
shddnrwls <shddnrwls@gmail.com>
2018-05-13 17:10:09 +0900

    DataType

---
+++ Datatype.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import Swift
+
+//Bool타입 True,False 값만 가질 수 있다
+var someBool : Bool = true
+someBool = false
+
+//Int타입 정수타입 현재는 기본적으로 64비트 정수형
+var age:Int = 30
+
+//Uint타입 양의정수 타입 64비트 양의 정수
+var friend:UInt = 4
+//friend = -1 //에러
+
+//Float타입 실수 타입,32비트 부동소수형
+var someFloat:Float = 3.14
+//Double타입 실수 타입,64비트 부동소수형
+var someDouble:Double = 5.5
+//someDouble = someFloat // 같은 실수형타입이지만 데이터타입이 달라 오류가난다.
+
+//Character 문자타입,유니코드사용,큰따옴표("")사용
+var someCharacter : Character = "힝"
+//Strig 문자열타입,유니코드사용,큰따옴표("")사용
+var someString: String = "하하호홓호호"
+//someString = someCharacter// 에러
+
+//Swift는 데이터타입에 민감하다
+
